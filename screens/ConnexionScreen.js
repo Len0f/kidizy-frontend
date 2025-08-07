1386c573fe22b07420a1a5c40edfd7c087e6bfa1
SylvainRichet1 <titisr36@hotmail.fr>
2025-08-07 15:24:56 +0200

    1

--- screens/ConnexionScreen.js
+++ screens/ConnexionScreen.js
@@ -5,7 +5,6 @@ import Input from '../components/Input';
 import SignBtn from '../components/signBtn';
 import { useState } from 'react';
 import { useSelector,useDispatch } from 'react-redux';
-import { updateInfo } from '../reducers/user';
 
 export default function ConnectionScreen({ navigation }) {
 
