f49526b2e8a014d968fa5ec5faf35763be271094
ErgoMundusDovi <eddylou91@gmail.com>
2025-08-14 15:33:20 +0200

    POST gardes

--- screens/PropositionScreen.js
+++ screens/PropositionScreen.js
@@ -179,7 +179,7 @@ export default function PropositionScreen({ navigation, route }) {
   // ------------------ BABYSITTER : MISE A JOUR DE LA PROPOSITION : ACCEPTED
   const accept = async () => {
     
-   const accepted =await fetch(`${url}propositions/id`, {
+   const accepted = await fetch(`${url}propositions/id`, {
         method: "PUT",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
@@ -189,8 +189,9 @@ export default function PropositionScreen({ navigation, route }) {
         })
       });
       const acceptRes= await accepted.json()
+      
       if (acceptRes.result){
-        const newConv= await fetch(`${url}conversations`, {
+        fetch(`${url}conversations`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
@@ -198,10 +199,20 @@ export default function PropositionScreen({ navigation, route }) {
         idUserParent: parentId,
         idUserBabysitter: user.selectedBabysitterId,
         updatedAt: new Date(),
-      }),
+      })
     })
-    const newConvRes= await newConv.json()
-    navigation.navigate('Dashboard')
+        fetch(`${url}gardes/new/id`,{
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+          token: user.token,
+          idUserParent: parentId,
+          idUserBabysitter: user.id,
+          proposition,
+          updatedAt: new Date(),
+      }),
+      })
+      navigation.navigate('Dashboard')
       }
     }
       // On marque la proposition comme acceptée
